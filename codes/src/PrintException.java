7170b1106b5344a64c7f35870790508a5ad16bb0
zhouxiaoqiang <zhouxiaoqiang@ycmedia.cn>
2015-08-24 13:30:54 +0800

    'add'

---
+++ codes/src/PrintException.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+/**
+ * Created by Administrator on 2015/8/24.
+ */
+public class PrintException {
+    public static String getExceptionMessage(Throwable exception) {
+        StringWriter out = new StringWriter();
+        exception.printStackTrace(new PrintWriter(out));
+        BufferedReader in = new BufferedReader(new StringReader(out.toString()));
+        try {
+            StringBuilder str = new StringBuilder();
+            for (; ; ) {
+                String line = in.readLine();
+                if (line == null) {
+                    break;
+                }
+                if (str.length() > 0) {
+                    str.append(", ");
+                }
+                str.append(line);
+            }
+            return str.toString();
+        } catch (IOException e) {
+            // ignore
+        }
+        return "n/a";
+    }
+}
